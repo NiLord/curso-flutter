11fb24b245b371ad796e7b3fbb4af93937d7cd42
NiLord <contacto@fernandogonzalez.dev>
2024-11-16 17:35:31 -0500

    74 - TextFormFields

--- yes_no_app/lib/main.dart
+++ yes_no_app/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: AppTheme(selectedColor: 0).theme(),
+      theme: AppTheme(selectedColor: 2).theme(),
       title: 'Yes No App',
       home: const ChatScreen(),
     );
--- yes_no_app/lib/presentation/screens/chat/chat_screen.dart
+++ yes_no_app/lib/presentation/screens/chat/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
 import 'package:yes_no_app/presentation/widgets/chat/other_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/shared/message_field_box.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -8,19 +9,31 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          leading: const Padding(
-            padding: EdgeInsets.all(4.0),
-            child: CircleAvatar(
-              backgroundImage: NetworkImage(
-                  "https://scontent.fpac2-2.fna.fbcdn.net/v/t39.30808-6/425386550_3202601386701548_358931755254050777_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeHCueDtFmBMzo7XSODRya3TITPIT0zr3OchM8hPTOvc5-IH4Z9vtMHP7lyGt79RWfA&_nc_ohc=V7nfGxHjLSkQ7kNvgHhvr03&_nc_pt=1&_nc_zt=23&_nc_ht=scontent.fpac2-2.fna&_nc_gid=AEAQzEcFwCZ3QZ5QYa1qqfv&oh=00_AYBnoWOiu8m7BDnJySDaLHut5qIreubyJTeM88C7WQkhAQ&oe=673ED6BA"),
-              backgroundColor: Colors.transparent,
-            ),
-          ),
-          title: const Text("Mi Neko Lindo 🥰"),
-          centerTitle: false),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(kToolbarHeight),
+        child: _ChatHeaderBar(),
+      ),
       body: _ChatView(),
     );
+  }
+}
+
+class _ChatHeaderBar extends StatelessWidget {
+  @override
+  /// Returns the top bar with a circular avatar and the title "Mi Neko Lindo"
+  ///
+  Widget build(BuildContext context) {
+    return AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+                "https://scontent.fpac2-2.fna.fbcdn.net/v/t39.30808-6/425386550_3202601386701548_358931755254050777_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeHCueDtFmBMzo7XSODRya3TITPIT0zr3OchM8hPTOvc5-IH4Z9vtMHP7lyGt79RWfA&_nc_ohc=V7nfGxHjLSkQ7kNvgHhvr03&_nc_pt=1&_nc_zt=23&_nc_ht=scontent.fpac2-2.fna&_nc_gid=AEAQzEcFwCZ3QZ5QYa1qqfv&oh=00_AYBnoWOiu8m7BDnJySDaLHut5qIreubyJTeM88C7WQkhAQ&oe=673ED6BA"),
+            backgroundColor: Colors.transparent,
+          ),
+        ),
+        title: const Text("Mi Neko Lindo 🥰"),
+        centerTitle: false);
   }
 }
 
@@ -36,7 +49,8 @@ class _ChatView extends StatelessWidget {
             itemCount: 100,
             itemBuilder: (context, index) => (index % 2 == 0) ? const OtherMessageBubble() : const MyMessageBubble(),
           )),
-          const Text("Mundo")
+          // Caja de texto
+          const MessageFieldBox()
         ]),
       ),
     );
