a1cc0293fa3033e4943e811ef7ac7e0855e5c7d5
NiLord <contacto@fernandogonzalez.dev>
2024-11-17 13:32:03 -0500

    88 - Models y Mappers

--- yes_no_app/lib/config/helpers/get_yes_no_answer.dart
+++ yes_no_app/lib/config/helpers/get_yes_no_answer.dart
@@ -1,13 +1,18 @@
 import 'package:dio/dio.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
+import 'package:yes_no_app/infraestructure/models/yes_no_model.dart';
 
 class GetYesNoAnswer {
   final _dio = Dio(BaseOptions(baseUrl: 'https://yesno.wtf/api'));
 
   Future<Message> getAnswer() async {
-
     final response = await _dio.get('/');
 
-    throw UnimplementedError();
+    final YesNoModel yesNoModel = YesNoModel.fromJsonMap(response.data);
+
+    return Message(
+        text: yesNoModel.answer,
+        fromWho: FromWho.other,
+        imageUrl: yesNoModel.image);
   }
 }
