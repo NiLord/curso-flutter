66f1c594269bf3689ad490a37a0f566624796c9e
NiLord <contacto@fernandogonzalez.dev>
2024-11-20 21:14:33 -0500

    99 - CircularProgress y Pageview

---
+++ toktik/lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:toktik/domain/entities/video_post.dart';
+
+class VideoScrollView extends StatelessWidget {
+  final List<VideoPost> videos;
+
+  const VideoScrollView({super.key, required this.videos});
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      physics: const BouncingScrollPhysics(),
+      children: [
+      Container(
+        color: Colors.red,
+      ),
+      Container(
+        color: Colors.blue,
+      ),
+      Container(
+        color: Colors.teal,
+      ),
+      Container(
+        color: Colors.yellow,
+      ),
+      Container(
+        color: Colors.deepPurple,
+      )
+    ]);
+  }
+}
