6cebbdc5a156ad9e8453bdf07133783d6a140642
NiLord <contacto@fernandogonzalez.dev>
2024-11-21 22:16:58 -0500

    106 y 107 - Pausar videos y reproducir videos

--- toktik/lib/presentation/widgets/video/fullscreen_player.dart
+++ toktik/lib/presentation/widgets/video/fullscreen_player.dart
@@ -19,7 +19,7 @@ class _FullscreenPlayerState extends State<FullscreenPlayer> {
   void initState() {
     super.initState();
 
-    controller = VideoPlayerController.asset(widget.videoUrl)
+    controller = VideoPlayerController.networkUrl(Uri.parse(widget.videoUrl))
       ..setVolume(0)
       ..setLooping(true)
       ..play();
@@ -36,11 +36,52 @@ class _FullscreenPlayerState extends State<FullscreenPlayer> {
     return FutureBuilder(
         future: controller.initialize(),
         builder: (context, snapshot) {
-          return const Center(
-            child: CircularProgressIndicator(
-              strokeWidth: 2,
-            ),
-          );
+          if (snapshot.connectionState == ConnectionState.done) {
+            return GestureDetector(
+              onTap: () => controller.value.isPlaying ? controller.pause() : controller.play(),
+              child: AspectRatio(
+                aspectRatio: controller.value.aspectRatio,
+                child: Stack(
+                  children: [
+                    VideoPlayer(controller),
+                    Positioned(
+                      bottom: 50,
+                      left: 20,
+                      child: _VideoCaption(caption: widget.caption),
+                    )
+                  ],
+                ),
+              ),
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(
+                strokeWidth: 2,
+              ),
+            );
+          }
         });
+  }
+}
+
+class _VideoCaption extends StatelessWidget {
+
+  final String caption;
+
+  const _VideoCaption({required this.caption});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final size = MediaQuery.of(context).size;
+    final titleStyle = Theme.of(context).textTheme.titleLarge;
+
+    return SizedBox(
+      width: size.width * 0.6,
+      child: Text( 
+        caption,
+        maxLines: 2,
+        style: titleStyle,
+      ));
   }
 }
--- toktik/lib/shared/data/local_video_posts.dart
+++ toktik/lib/shared/data/local_video_posts.dart
@@ -1,49 +1,49 @@
 List<Map<String, dynamic>> videoPosts = [
   {
     'name': 'Subiendo escaleras automáticas',
-    'videoUrl': 'assets/videos/1.mp4',
+    'videoUrl': 'https://videos.pexels.com/video-files/4678261/4678261-hd_1080_1920_25fps.mp4',
     'likes': 23230,
     'views': 1523,
   },
   {
     'name': 'Planta apreciada por peatones',
-    'videoUrl': 'assets/videos/2.mp4',
+    'videoUrl': 'https://videos.pexels.com/video-files/4434242/4434242-hd_1080_1920_24fps.mp4',
     'likes': 24230,
     'views': 1343,
   },
   {
     'name': 'Que borroso veo todo!',
-    'videoUrl': 'assets/videos/3.mp4',
+    'videoUrl': 'https://videos.pexels.com/video-files/3992630/3992630-uhd_1440_2732_25fps.mp4',
     'likes': 21564320,
     'views': 123563,
   },
   {
     'name': '¿Esto es trigo? que interesante',
-    'videoUrl': 'assets/videos/4.mp4',
+    'videoUrl': 'https://videos.pexels.com/video-files/4434150/4434150-hd_1080_1920_30fps.mp4',
     'likes': 320,
     'views': 2300,
   },
   {
     'name': 'El COVID no me afecta',
-    'videoUrl': 'assets/videos/5.mp4',
+    'videoUrl': 'https://videos.pexels.com/video-files/3959544/3959544-uhd_1440_2732_25fps.mp4',
     'likes': 3230,
     'views': 31030,
   },
   {
     'name': 'No quiero ir a trabajar hoy señor Stark',
-    'videoUrl': 'assets/videos/6.mp4',
+    'videoUrl': 'https://videos.pexels.com/video-files/4057150/4057150-uhd_1440_2732_25fps.mp4',
     'likes': 10,
     'views': 330,
   },
   {
     'name': 'Limpiar nunca fue tan divertido',
-    'videoUrl': 'assets/videos/7.mp4',
+    'videoUrl': 'https://videos.pexels.com/video-files/5595352/5595352-hd_1080_1920_24fps.mp4',
     'likes': 1320,
     'views': 33032,
   },
   {
     'name': '¿Ya llegamos a la India?... umm si',
-    'videoUrl': 'assets/videos/8.mp4',
+    'videoUrl': 'https://videos.pexels.com/video-files/4065630/4065630-uhd_1440_2732_25fps.mp4',
     'likes': 342,
     'views': 3332,
   }
